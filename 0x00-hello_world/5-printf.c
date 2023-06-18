54aa162fab8643d4675a5bd9d204509c4aa55e87
Bless50 <blessn691@gmail.com>
2023-06-18 11:05:25 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints a string using puts function
+ * Return: Always 0 (Success)
+ */
+int main(viod)
+{
+ptintf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
