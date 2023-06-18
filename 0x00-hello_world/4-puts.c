bb5010e1b165be9e10a32a78bcd4a02d0fe31984
Bless50 <blessn691@gmail.com>
2023-06-18 10:39:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-return puts("Programming is like building a multilingual puzzle");
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description: Prints a string using puts function
+ * Return: Always 0 (Success)
+ */
+int maian(void)
+{
+	puts("Programming is like building a multilingual puzzle"\n);
+         return (0);
+}
