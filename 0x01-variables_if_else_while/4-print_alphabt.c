215303b7840b80b64a44ca3c085aa542c4db664b
Bless50 <blessn691@gmail.com>
2023-06-22 04:14:52 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the letters of the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+if(i == 'q' && i == 'e')
+continue;
+}
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
