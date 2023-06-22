6281d59847ca1019288e85c533b20b111938ba9a
Bless50 <blessn691@gmail.com>
2023-06-22 15:33:04 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 8-print_base16.c -o 8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the letters of the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+char n;
+
+for (i = 0; i <= 9; i++)
+putchar(i);
+for (n = 'a'; i <= 'f'; i++)
+putchar(n);
+putchar('\n');
+
+return (0);
+}
