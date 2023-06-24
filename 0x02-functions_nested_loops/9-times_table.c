2b49fc62bc9bd804ce37216cf004a7ab42ef3fab
Bless50 <blessn691@gmail.com>
2023-06-24 11:47:08 -0700

    u+x  9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main"
+#include <stdio.h>
+
+/**
+ * times_table - Prints the times table from 0 to 99.
+ *
+ * Return: Nothing.
+ */
+void times_table(void)
+{
+int i, j;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+putchar(i * j + '0');
+if (j < 9)
+{
+putchar(',');
+}
+}
+putchar('\n');
+}
+}
