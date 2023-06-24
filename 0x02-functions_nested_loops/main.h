db0b659d4946c93c9465fc95af071d139886323f
Bless50 <blessn691@gmail.com>
2023-06-23 20:17:15 -0700

    #ifndef MAIN_H #define MAIN_H
    void print_alphabet(void);
    void _putchar(char c);
    void print_alphabet_x10(void);
    int _islower(int c);
    int _isalpha(int c);
    void print_to_98(int n);
    int add(int, int);
    int print_sign(int n);
    
    #endif

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_alphabet(void);
+void _putchar(char c);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+void print_to_98(int n);
+int add(int, int);
+int print_sign(int n);
+
+#endif
