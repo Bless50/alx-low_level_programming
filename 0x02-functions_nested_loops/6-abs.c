2ba543b9913f870a4d0b6e187abba8cba4c9854d
Bless50 <blessn691@gmail.com>
2023-06-24 09:29:31 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Description:  Printing the absolute value of an integer
+ *
+ * Return - Always 0.
+ */
+int _abs(int x) 
+{
+
+if (x < 0)
+{
+return (-x);
+}
+else
+{
+return (x);
+}
+}
