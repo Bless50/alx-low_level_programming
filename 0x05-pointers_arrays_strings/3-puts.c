19c6497466d1b8d987b82202f7afef2229747977
Bless50 <blessn691@gmail.com>
2023-06-26 17:31:50 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,17 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _puts - Description:return the value of a string
  *@str: the variable.
- * Return: string
+ * 
  */
+
 void _puts(char *str)
 {
-return (str);
+char i = 0;
+while (str[i] != '\0')
+{
+printf(" %c", str[i]);
+i++;
 }
 
